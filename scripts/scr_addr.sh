fe3964f4451324cb05d94e596e6eb34a72928218
tknott95 <tknott95@hotmail.com>
2022-09-11 04:10:01 -0600

    scripts

--- scripts/scr_addr.sh
+++ scripts/scr_addr.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-```
+: '
  $1 is "loc of file" 
  EX: datum-redeemer.plutus
 
  $2 is .addr file out
  EX: datum-redeemer.addr
-```
 
-cardano-cli address build --payment-script-file $1  --mainnet --out-file $2
+ EXAMPLE OF SCRIPT RUNNING:
+  ./scr_addr.sh ../compiled-scripts/customDtm137-v2.plutus customDtm137.addr
+'
+
+cardano-cli address build --payment-script-file $1  --mainnet --out-file ../crypt/addrs/$2
