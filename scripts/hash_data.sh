2d5bc06023cbb417a71060e535285efa6eac5b2c
Trevor Knott <Tknott95@hotmail.com>
2022-09-13 00:04:02 -0600

    Hashing Scripts

--- scripts/hash_data.sh
+++ scripts/hash_data.sh
@@ -7,6 +7,9 @@
 
  REF TO MAKE JSON AS OBJECT PROPER: https://forum.cardano.org/t/cardano-cli-hash-script-data/80770/4 */
 
+
+ EXAMPLE:
+  ./hash_data.sh test_cmplx.json
 '
 
 cardano-cli transaction hash-script-data --script-data-file $1
--- scripts/hash_val.sh
+++ scripts/hash_val.sh
@@ -4,6 +4,9 @@
  $1 is an integer 
 
  takes in an integer and returns a hash
+
+ EXAMPLE:
+  ./hash_data.sh 137
 '
 
 cardano-cli transaction hash-script-data --script-data-value $1
