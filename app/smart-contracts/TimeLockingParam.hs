d0724cfb5f8781df15d3c67b683b0296bdac6de7
tknott95 <tknott95@hotmail.com>
2022-09-18 00:01:29 -0600

    notes/hotfix | TimeLockingParam.hs

--- app/smart-contracts/TimeLockingParam.hs
+++ app/smart-contracts/TimeLockingParam.hs
@@ -60,7 +60,8 @@ mockData :: TimeLockingParam
 mockData = TimeLockingParam {
   -- hard coded pub key and time to check against
   beneficiaryOfFunds = "3f7846896a48c59359746ff096d63606ceb82e65900d20a9fd2b8a93",
-  whenAvailable = 1596059095000
+  whenAvailable = 1660802437
+  -- 8/18/22
 }
 --  where
 --    mockTime :: POSIXTime
